49b140710074e19eee71e3c6547c800b2338e849
RamiNaim <rami.naim2010@yandex.ru>
2018-09-10 12:57:54 +0300

    Lab1: comments added, small fixes

--- Lab1/Task-1/faceted_search.py
+++ Lab1/Task-1/faceted_search.py
@@ -4,15 +4,15 @@ from copy import deepcopy
 def start():
 	init = []
 
-	# Заполняем лист init разными птицами
+	# Заполняем лист init разными птицами (16 штук) с уникальной комбинацией атрибутов
 	eagle = Bird("Орел", size="средняя", color="не яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="является символом Америки")
 	init.append(eagle)
 
 	turkey = Bird("Индейка", size="средняя", color="не яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России")
 	init.append(turkey)
 
-	pavlin = Bird("Павлин", size="большая", color="яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России", sq="обладает очень красивым хвостом")
-	init.append(pavlin)
+	peacock = Bird("Павлин", size="большая", color="яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России", sq="обладает очень красивым хвостом")
+	init.append(peacock)
 
 	canary = Bird("Канарейка", size="маленькая", color="яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="чувствительна к запаху газа")
 	init.append(canary)
@@ -26,8 +26,8 @@ def start():
 	duck = Bird("Утка", size="средняя", color="не яркая", water="водоплавающая", wildRussia="встречается в дикой природе России")
 	init.append(duck)
 
-	straus = Bird("Страус", size="большая", color="не яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России")
-	init.append(straus)
+	ostrich = Bird("Страус", size="большая", color="не яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России")
+	init.append(ostrich)
 
 	swan = Bird("Лебедь", size="большая", color="не яркая", water="водоплавающая", wildRussia="встречается в дикой природе России")
 	init.append(swan)
@@ -35,23 +35,23 @@ def start():
 	parrot = Bird("Попугай Ара", size="большая", color="яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России", sq="разговаривает")
 	init.append(parrot)
 
-	penguen = Bird("Пингвин", size="большая", color="не яркая", water="водоплавающая", wildRussia="не встречается в дикой природе России")
-	init.append(penguen)
+	penguin = Bird("Пингвин", size="большая", color="не яркая", water="водоплавающая", wildRussia="не встречается в дикой природе России")
+	init.append(penguin)
 
-	woodpeeker = Bird("Дятел", size="средняя", color="не яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="делает дупло?")
-	init.append(woodpeeker)
+	woodpecker = Bird("Дятел", size="средняя", color="не яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="делает дупло?")
+	init.append(woodpecker)
 
-	kukushka = Bird("Кукушка", size="средняя", color="не яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="оставляет своих птенцов")
-	init.append(kukushka)
+	cuckoo = Bird("Кукушка", size="средняя", color="не яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="оставляет своих птенцов")
+	init.append(cuckoo)
 
 	flamingo = Bird("Фламинго", size="большая", color="яркая", water="водоплавающая", wildRussia="не встречается в дикой природе России")
 	init.append(flamingo)
 
-	calibry = Bird("Каллибри", size="маленькая", color="яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России", sq="может летать назад")
-	init.append(calibry)
+	colibri = Bird("Калибри", size="маленькая", color="яркая", water="неводоплавающая", wildRussia="не встречается в дикой природе России", sq="может летать назад")
+	init.append(colibri)
 
-	snegir = Bird("Снегирь", size="маленькая", color="яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="любит кушать рябину зимой")
-	init.append(snegir)
+	bullfinch = Bird("Снегирь", size="маленькая", color="яркая", water="неводоплавающая", wildRussia="встречается в дикой природе России", sq="любит кушать рябину зимой")
+	init.append(bullfinch)
 
 
 	facered_search(deepcopy(init))
@@ -59,40 +59,41 @@ def start():
 
 
 def facered_search(init_list):
-	filtred = init_list
-
-	user_answer = "Нет"
+	# Счетцик, который показывает, какой атрибут мы определяем
 	number_of_attributes = 0
-
-	picked_attribute = None
 
 	while len(init_list) != 1:
 
 		possible_attributes = []
 
+		# Фильтруем по размеру
 		if number_of_attributes == 0:
+
+			# заполняем лист possible_attributes возможными значениями параметра
 			for bird in init_list:
 				if bird.size not in possible_attributes:
 					possible_attributes.append(bird.size)
 
+			# задаем пользователю вопрос до тех пор, пока не останется один возможный атрибут или пользователь ответит Да
 			for i in range( len(possible_attributes) - 1 ):
 				print( "Ваша птица " + possible_attributes[i] + "?" )
 				user_answer = input()
 				if user_answer == "Да":
 					index = 0
+					# Оставляем в possible_attributes то значение, которое выбрал пользователь
 					for _ in range(0, len(init_list) ):
 						if init_list[index].size != possible_attributes[i]:
 							init_list.pop(index)
+							# Если удалили один элемент, уменьшаем индекс на 1
 							index -= 1
 						index += 1
-						if index == len( init_list ):
-							break
-
 					break
 				else: 
 					possible_attributes.pop(i)
 
+			# если остается одно возможное значение, удаляем из init_list все неподходящие значения
 			if len( possible_attributes ) == 1:
+				print("Тогда ваша птица " + possible_attributes[0])
 				index = 0
 				for _ in range(0, len(init_list) ):
 					if init_list[index].size != possible_attributes[0]:
@@ -102,6 +103,7 @@ def facered_search(init_list):
 					if index == len( init_list ):
 						break
 
+		# Аналогично проверяем окрас
 		elif number_of_attributes == 1:
 			for bird in init_list:
 				if bird.brightColor not in possible_attributes:
@@ -134,7 +136,7 @@ def facered_search(init_list):
 					if index == len( init_list ):
 						break
 
-
+		# проверяем, загаданная птица поавает в воде или нет
 		elif number_of_attributes == 2:
 			for bird in init_list:
 				if bird.waterfowl not in possible_attributes:
@@ -167,7 +169,7 @@ def facered_search(init_list):
 					if index == len( init_list ):
 						break
 
-
+		# Проверяем, встречается ли эта птица в дикой природе России
 		elif number_of_attributes == 3:
 			for bird in init_list:
 				if bird.wildInRussia not in possible_attributes:
@@ -200,7 +202,7 @@ def facered_search(init_list):
 					if index == len( init_list ):
 						break
 
-
+		# если предыдущих действий не хватило для определения одной птицы, задаем спец. вопрос (Bird.specialQuality)
 		elif number_of_attributes == 4:
 
 			for bird in init_list:
@@ -237,8 +239,8 @@ def facered_search(init_list):
 
 		number_of_attributes += 1
 
-
-	print ( init_list[0] )
+	# Выводим отфильтрованный список. в котором находится один элемент
+	print( "Под данные параметры подходит одна птица: " + str(init_list[0]) )
 
 
 if __name__ == '__main__':
--- Lab1/Task-2/numbers_parse.py
+++ Lab1/Task-2/numbers_parse.py
@@ -1,11 +1,15 @@
+"""Функция просит пользователя ввести числа, затем разделяет строку на отдельные числа"""
 def get_numbers():
 	numbers_srt = input("Введите числа через запятую (после запятой пробел, десятичный разделитель - точка, косплексный вид: a+bj):\n")
 	print()
+
+	"""Разделяем строку на одельны числа"""
 	numbers = numbers_srt.split(", ")
 	for num in numbers:
+		"""Передаем каждое число в виде строки в функцию, которая определяет принадлежность числа к определенной группе"""
 		parse_number(num)
-		#print( num + " - " + parse_number(num) )
 
+	"""Вывод результата"""
 	print("Комплексные: " + list_to_str(comp))
 	print("Рациональные: " + list_to_str(rational))
 	print("Целые: " + list_to_str(integer))
@@ -14,7 +18,7 @@ def get_numbers():
 	print("Нечетные: " + list_to_str(odd))
 	print("Простые: " + list_to_str(prime_num))
 
-
+"""Создаем пустые массивы(list) для каждой из групп"""
 comp=[]
 rational=[]
 natural=[]
@@ -23,47 +27,46 @@ even=[]
 odd=[]
 prime_num=[]
 
+"""Функция принимает число в виде строки, и помещает число в соответствующую группу"""
 def parse_number(n):
 	n = complex(n)
 	if n.imag != 0:
-		#return "комплексное"
 		comp.append(n)
+		"""Дальше функция не выполняется"""
 		return
 	else:
 		n = float(n.real)
 
 	if n % 1 != 0:
-		#return "рациональное"
 		rational.append(n)
 		return
 	else:
 		n = int(n)
 
-	result = ""
 	if n <= 0:
-		#result += "целое"
 		integer.append(n)
 	else:
-		#result += "натуральное"
-		natural.append(n)
 		integer.append(n)
+		natural.append(n)
+		"""Если число натуральное, то необходимо проверить его на простоту"""
 		if prime(n):
-			#result += ", простое"
 			prime_num.append(n)
 
+	"""Проверка на четность"""
 	if n % 2 == 0:
-		#result += ", четное"
 		even.append(n)
 	else:
-		#result += ", нечетное"
 		odd.append(n)
 
 	return
 
+"""Функция принимает в виде аргумента число и возвращает True, если число простое, иначе - False"""
 def prime(n):
+	"""Если передана едининца - возвращаем 1"""
 	if n == 1:
 		return False
 
+		"""Достаточно проверить, являются ли все числа от 2 до n/2 (округление в большую сторону), делителями числа n"""
 	top_lim = n // 2 + 1
 	for den in range(2, top_lim):
 		if n % den == 0:
@@ -71,6 +74,7 @@ def prime(n):
 
 	return True
 
+"""Функция переводит list в строку"""
 def list_to_str(l):
 	s = ""
 	for elem in l:
