d69cb2695130f1372e554a3c2c5bd3078e4f55e6
RamiNaim <rami.naim2010@yandex.ru>
2018-09-14 22:20:48 +0300

    Lab1: task3 fixed

--- Lab1/Task-3/timeTest.py
+++ Lab1/Task-3/timeTest.py
@@ -5,32 +5,37 @@ from copy import deepcopy as dp
 random.seed( time.time() )
 
 data = open("data.txt", "w")
+heapTime = dwarfTime = bubbleTime = blockTime = 0
 
 for k in range(100):
-	array = random.sample( range(1, 101), 10 )
+	array = random.sample( range(1, 1000), 100 )
 
 	time1 = time.time()
 	heapSort( dp(array) )
-	heapTime = time.time() - time1
+	d1 = time.time() - time1
+	heapTime += d1
 
 	time1 = time.time()
 	dwarfSort( dp(array) )
-	dwarfTime = time.time() - time1
+	d2 = time.time() - time1
+	dwarfTime += d2
 
 	time1 = time.time()
 	bubbleSort( dp(array) )
-	bubbleTime = time.time() - time1
+	d3 = time.time() - time1
+	bubbleTime += d3
 
 	time1 = time.time()
 	blockSort( dp(array) )
-	blockTime = time.time() - time1
+	d4 = time.time() - time1
+	blockTime += d4
 
-	data.write( str(k) + " " + str(heapTime * 1000 * 1000) + " " + str(dwarfTime * 1000 * 1000) + " " + str(bubbleTime * 1000 * 1000) + " " + str(blockTime * 1000 * 1000) + "\n"  )
+	data.write( str(k) + " " + str(d1 * 1000 * 1000) + " " + str(d2 * 1000 * 1000) + " " + str(d3 * 1000 * 1000) + " " + str(d4 * 1000 * 1000) + "\n"  )
 
-print( "Среднее время сортировки массива из 10 элементов (в нс)" )
-print( "Кучей:       " + str(heapTime * 1000 * 1000) )
-print( "Гномья:      " + str(dwarfTime * 1000 * 1000) )
-print( "Пузырьковая: " + str(bubbleTime * 1000 * 1000) )
-print( "Блочная:     " + str(blockTime * 1000 * 1000) )
+print( "Среднее время сортировки массива из 100 элементов (в нс)" )
+print( "Кучей:       " + str(heapTime * 1000 * 1000 / 100) )
+print( "Гномья:      " + str(dwarfTime * 1000 * 1000 / 100) )
+print( "Пузырьковая: " + str(bubbleTime * 1000 * 1000 / 100) )
+print( "Блочная:     " + str(blockTime * 1000 * 1000 / 100) )
 
 data.close()
