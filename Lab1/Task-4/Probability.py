5019f6782b65da597f754b8e99b701d29869f5dc
RamiNaim <rami.naim2010@yandex.ru>
2018-09-21 00:14:33 +0300

    Task-1 finished

--- Lab1/Task-4/Probability.py
+++ Lab1/Task-4/Probability.py
@@ -1,8 +1,8 @@
 print("Введите три вероятноcти для страшного оползня: для Никиты, Сережи и Насти.")
 print("P.S.Ввероятность может принимать значения от 0 до 1")
-Nicita=float(input())
-Serezha=float(input())
-Nastya=float(input())
+Nicita=float(input("Никита: "))
+Serezha=float(input("Сережа: "))
+Nastya=float(input("Настя: "))
 C = 14
 D = 200
 if ((Nicita > 1) or (Serezha > 1) or (Nastya > 1) or (Nicita < 0) or (Serezha < 0) or (Nastya < 0)):
@@ -13,5 +13,5 @@ else:
         # Округляем ответ
         Answer = round(Answer, 3)
         # Выводим ответ
-        print(Answer)
+        print("Вероятность того, что за 200 дней оползень произойдет только с Никитой: " + str(Answer) )
 
--- Lab2/Task-1/InitTable.py
+++ Lab2/Task-1/InitTable.py
@@ -1,15 +1,29 @@
 from openpyxl import Workbook
-import random
+import random, time
+import numpy as np
+import matplotlib.pyplot as plt
 
-random.seed()
 
-def initTable():
+
+
+random.seed( time.time() )
+
+def start():
+	limits = input("Укажите максимальное и минимальное значение случайной величины\n(Например: 0, 20):\n")
+
+	min, max = limits.split(", ")
+	min = float(min)
+	max = float(max)
+
+
+
+def initTable(min=-20, max=20):
 
 	wb = Workbook()
 	ws = wb.active
 
-	upper_limit = 20
-	lower_limit = -20
+	upper_limit = max
+	lower_limit = min
 
 	coords = set()
 
@@ -29,7 +43,53 @@ def initTable():
 
 	wb.save("Data.xlsx")
 
+	MNK(coords)
+
+
+def MNK(data):
+
+	fig = plt.figure()
+
+	n = len(data)
+
+	sumx = 0
+	sumx2 = 0
+	sumy = 0
+	sumxy = 0
+
+	x_coords = []
+	y_coords = []
+
+	for point in data:
+
+		plt.scatter(point[0], point[1])
+
+		sumx += point[0]
+		sumy += point[1]
+
+		sumxy += point[0] * point[1]
+		sumx2 += point[0] ** 2
+
+		x_coords.append( point[0] )
+
+
+	a = ( n * sumxy - sumx * sumy ) / ( n * sumx2 - sumx ** 2 )
+	b = 1.0 * ( sumy - a * sumx ) / n
+
+
+	for x in x_coords:
+		y_coords.append( a * x + b )
+		plt.scatter(x, a * x + b)
+	
+	plt.plot(x_coords, y_coords)
+	plt.ylabel('Y')
+	plt.xlabel('X')
+	plt.show()
+
+
+
+
 
 
 if __name__ == '__main__':
-	initTable()
+	start()
